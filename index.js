88d730842853a5ab6f047aee2f1cec4ec60b0fd5
Gopalakrishnan <krishnan.gopal34@gmail.com>
2020-05-27 00:58:24 +0530

    moved private info into dev.js

--- index.js
+++ index.js
@@ -2,16 +2,38 @@ const express = require('express')
 
 const app = express();
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://gopal:gopal%40123@cluster0-x5k2e.gcp.mongodb.net/test?retryWrites=true&w=majority', 
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+
+const config = require('./config/key')
+
+const { User } = require('./models/user')
+
+
+mongoose.connect(config.mongoURI, 
 {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log("DB Connected"))
     .catch(err => console.error(err))
 
 
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(cookieParser())
 
 
-app.get('/', (req, res) => {
-    res.send("Hello World")
+
+
+app.post('/api/users/register', (req, res) => {
+    const user = new User(req.body)
+    user.save((err, userData) => {
+        if(err) return res.json({success: false, err})
+
+        return res.status(200).json({
+            success: true
+        })
+    })
+
+    
 })
 
 app.listen(5000);
